a765daf0cc736fc3a1dff6aa67d0b87c34c7731e
David Feng <fenghourun@gmail.com>
2024-10-09 17:21:24 -0400

    Update theme:

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.window_decorations = "NONE"
 
 
