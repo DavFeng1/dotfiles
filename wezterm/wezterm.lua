0169b794087e9ada245d07ad7033d513e3b38f48
David Feng <fenghourun@gmail.com>
2024-10-09 16:38:58 -0400

    Add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+
+config.keys = {
+  {
+    key = 'r',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.ReloadConfiguration,
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
