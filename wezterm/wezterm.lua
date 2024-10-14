757fbaa4d7645b84a65b3df8cd906fd386e14c32
David Feng <fenghourun@gmail.com>
2024-10-14 18:52:21 -0400

    Toggling wezterm color

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,10 +5,45 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Gruvbox dark, hard (base16)'
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Gruvbox dark, hard (base16)'
+  else
+    return 'Builtin Solarized Light'
+  end
+end
+
+function colors_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return {
+      background = "#1e2021",
+      tab_bar = {
+        background = "#1e2021"
+      },
+    }
+  else
+    return {
+      background = "#f5f5f5",
+      tab_bar = {
+        background = "#f5f5f5"
+      },
+    }
+  end
+end
+
+local appearance = get_appearance()
+config.colors = colors_for_appearance(appearance)
+config.color_scheme = scheme_for_appearance(appearance)
+
 config.window_decorations = "RESIZE"
-
-
 config.keys = {
   {
     key = 'r',
@@ -17,12 +52,6 @@ config.keys = {
   },
 }
 
-config.colors = {
-  background = "#1e2021",
-  tab_bar = {
-    background = "#1e2021"
-  },
-}
 
 config.font_size = 18
 config.use_fancy_tab_bar = false
